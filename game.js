da7447192021ea91e829aea09449ec3545336c4f
anthonyp1775 <anthony.pino@peopleshores.com>
2026-07-28 16:52:07 -0400

    recreated using character class

--- game.js
+++ game.js
@@ -1,32 +1,34 @@
-const adventurer = {
-  name: "Shogun",
-
-  health: 10,
-
-  inventory: ["Katana", "potion", "artifact"],
-
-  companion: {
-    name: "Leo",
-        subCompanion: {
-        name: "Frank",
-        type: "Flea",
-        inventory: ["Small Hat", "Sunglasses"]
-    },
-    type: "Cat"
-  },
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
 
   roll(mod = 0) {
     const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`)
+    console.log(`${this.name} rolled a ${result}.`);
   }
+}
+
+const shogun = new Character("Shogun");
+shogun.health = 10;
+shogun.inventory = ["Katana", "potion", "artifact"];
+shogun.companion = {
+  name: "Leo",
+  subCompanion: {
+    name: "Frank",
+    type: "Flea",
+    inventory: ["Small Hat", "Sunglasses"]
+  },
+  type: "Cat"
 };
-adventurer.inventory[0];
-console.log("The Shogun's primary weapon is the " + adventurer.inventory[0]);
+
+console.log(`The Shogun's primary weapon is the ${shogun.inventory[0]}`);
 
 const inventoryItems = ["Katana", "potion", "artifact"];
-
 inventoryItems.forEach((item) => {
   console.log(item);
 });
 
-return adventurer.roll()
+shogun.roll();
