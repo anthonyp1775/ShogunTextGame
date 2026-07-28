b0344c3904e8e0d7df747f1f71b7fcb7247cce80
anthonyp1775 <anthony.pino@peopleshores.com>
2026-07-28 15:47:42 -0400

    funtion loop added that logs the Shogun's inventory

--- game.js
+++ game.js
@@ -10,3 +10,9 @@ inventory: ["Katana", "potion", "artifact"]
 
 adventurer.inventory[0]
 console.log("The Shogun's primary weapon is the " + adventurer.inventory[0]);
+
+const inventoryItems = ["Katana", "potion", "artifact"];
+
+inventoryItems.forEach(item => {
+    console.log(item);
+});
