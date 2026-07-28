f34f286b92ba94c1daa3ced887b2511110ef9087
anthonyp1775 <anthony.pino@peopleshores.com>
2026-07-28 16:37:57 -0400

    working dice roll

--- game.js
+++ game.js
@@ -1,18 +1,32 @@
 const adventurer = {
+  name: "Shogun",
 
-name: "Shogun",
+  health: 10,
 
-health: 10,
+  inventory: ["Katana", "potion", "artifact"],
 
-inventory: ["Katana", "potion", "artifact"]
+  companion: {
+    name: "Leo",
+        subCompanion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["Small Hat", "Sunglasses"]
+    },
+    type: "Cat"
+  },
 
-}
-
-adventurer.inventory[0]
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+  }
+};
+adventurer.inventory[0];
 console.log("The Shogun's primary weapon is the " + adventurer.inventory[0]);
 
 const inventoryItems = ["Katana", "potion", "artifact"];
 
-inventoryItems.forEach(item => {
-    console.log(item);
+inventoryItems.forEach((item) => {
+  console.log(item);
 });
+
+return adventurer.roll()
