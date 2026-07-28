10558b771d196b41787619580aa3a1d4f1c8af2b
anthonyp1775 <anthony.pino@peopleshores.com>
2026-07-28 15:37:15 -0400

    first commit with working introduction of primary weapon of the Shogun

---
+++ game.js
@@ -0,0 +1,12 @@
+const adventurer = {
+
+name: "Shogun",
+
+health: 10,
+
+inventory: ["Katana", "potion", "artifact"]
+
+}
+
+adventurer.inventory[0]
+console.log("The Shogun's primary weapon is the " + adventurer.inventory[0]);
