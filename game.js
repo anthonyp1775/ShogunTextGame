8fd95761f12aed6324d150ae4f398438c23280d5
anthonyp1775 <anthony.pino@peopleshores.com>
2026-07-28 20:09:03 -0400

    duel aspect working

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 class Character {
+  static MAX_HEALTH = 100;
+
   constructor(name) {
     this.name = name;
     this.health = 100;
@@ -8,6 +10,7 @@ class Character {
   roll(mod = 0) {
     const result = Math.floor(Math.random() * 20) + 1 + mod;
     console.log(`${this.name} rolled a ${result}.`);
+    return result;
   }
 }
 
@@ -32,3 +35,83 @@ inventoryItems.forEach((item) => {
 });
 
 shogun.roll();
+
+class Adventurer extends Character {
+  static ROLES = ["Fighter", "Healer", "Wizard"];
+
+  constructor (name, role) {
+
+    super(name);
+
+    if (!Adventurer.ROLES.includes(role)) {
+      throw new Error(`${role} is not a valid role. Must be one of: ${Adventurer.ROLES.join(", ")}`);
+    }
+
+    this.role = role;
+
+
+    this.inventory.push("bedroll", "50 gold coins");
+
+  }
+
+ 
+
+  scout () {
+
+    console.log(`${this.name} is scouting ahead...`);
+
+    super.roll();
+
+  }
+
+  duel (opponent) {
+
+    console.log(`${this.name} challenges ${opponent.name} to a duel!`);
+
+    while (this.health > 50 && opponent.health > 50) {
+
+      const myRoll = this.roll();
+      const opponentRoll = opponent.roll();
+
+      if (myRoll < opponentRoll) {
+        this.health -= 1;
+      } else if (opponentRoll < myRoll) {
+        opponent.health -= 1;
+      }
+
+      console.log(`Round result: ${this.name} rolled ${myRoll} (health: ${this.health}), ${opponent.name} rolled ${opponentRoll} (health: ${opponent.health}).`);
+
+    }
+
+    const winner = this.health > 50 ? this : opponent;
+    console.log(`${winner.name} wins the duel!`);
+
+    return winner;
+
+  }
+
+}
+
+class AdventurerFactory {  
+  constructor (role) {
+    this.role = role;
+    this.adventurers = [];
+  }
+  generate (name) {
+    const newAdventurer = new Adventurer(name, this.role);
+    this.adventurers.push(newAdventurer);
+  }
+  findByIndex (index) {
+    return this.adventurers[index];
+  }
+  findByName (name) {
+    return this.adventurers.find((a) => a.name === name);
+  }
+}
+
+const healers = new AdventurerFactory("Healer");
+const robin = healers.generate("Showgun");
+
+const aria = new Adventurer("Aria", "Fighter");
+const bram = new Adventurer("Bram", "Healer");
+aria.duel(bram);
